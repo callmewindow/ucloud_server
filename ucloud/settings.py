af0ca418c7a3925d3f6e89890622014a334f43ea
里泰然 <5583226+li_tai_ran@user.noreply.gitee.com>
2020-09-23 17:02:23 +0800

    复制及修改

--- ucloud/settings.py
+++ ucloud/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ucloud',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'Beacon123!',
         'HOST': ''
     }
 }
