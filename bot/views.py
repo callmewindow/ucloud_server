32c4cfd0d5f5c498849e3ddb30803d7d97ad22c5
里泰然 <5583226+li_tai_ran@user.noreply.gitee.com>
2020-09-29 23:56:15 +0800

    日志文件测试

--- bot/views.py
+++ bot/views.py
@@ -262,7 +262,7 @@ def get_last_n_lines(logfile, n):
             fp.seek(cur_pos - blk_size, os.SEEK_SET)
             blk_data = fp.read(blk_size)
             assert len(blk_data) == blk_size
-            blk_data = str(blk_data)
+            blk_data = str(blk_data,encoding='utf8')
             lines = blk_data.split('\n')
 
             # adjust cur_pos
